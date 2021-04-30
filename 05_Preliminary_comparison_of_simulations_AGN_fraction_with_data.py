corrected f_p calculation

05_Preliminary_comparison_of_simulations_AGN_fraction_with_data.py
@@ -49,24 +49,12 @@ import plotting_cswl05 as pt
 # look back into redshifts until...
 redshift_limit = 2
 
-# agn parameters
-agn_FX_soft = 0
-
-# galaxy parameters
-galaxy_SMHMR_mass = 9 # unit: log 10, M_solar
-
-# halo parameters
-halo_mass_500c = 10**13.7 # solar masses
-central_Mvir = 13.7 # unit: log 10, M_solar
-cluster_params = [halo_mass_500c, central_Mvir]
-
 # pixel number from the simulation file
 pixel_no = '000000'
 
 # Define cosmology used in the notebook
 cosmo = FlatLambdaCDM(H0=67.77*u.km/u.s/u.Mpc, Om0=0.307115)
 h = 0.6777
-L_box = 1000.0 / h
 
 """
 2. Parameters for creating the Major Merger catalogue
@@ -87,52 +75,60 @@ mass_max = 3
 dv_cut =  500
 
 # BOOLEAN: if the pairs have already been computed before
-run_find_pairs = False
+run_find_pairs = True
+run_merger_pairs = False
 
 # BOOLEAN: if we want to find all pairs (not just major pairs)
 major_mergers_only = True
+delta_v_cut = True
 
 """
 3. Open files and get relevant data
 """
-hd_agn, hd_halo, _ = edh.getHeaders(pixel_no, np.array(['agn', 'halo']))
+_, hd_halo, _ = edh.getHeaders(pixel_no, np.array([ 'halo']))
 
-# Extracting positions and redshifts of the AGNs, galaxies, and halos
-# agns
-pos_z_AGN, _, conditions_agn = edh.getAgnData(hd_agn, agn_FX_soft, redshift_limit)    
+# Extracting positions and redshifts of the halos
+_, _, conditions_halo = edh.getGalaxyData(hd_halo, '', redshift_limit)
+hd_halo_z = hd_halo[conditions_halo]
 
-# galaxies and halos
-pos_z_gal, _, conditions_gal = edh.getGalaxyData(hd_halo, galaxy_SMHMR_mass, redshift_limit)
-pos_z_halo, _, conditions_halo = edh.getGalaxyData(hd_halo, '', redshift_limit)
-
-print("AGNs: %d, Galaxies: %d, Halos: %d"%(len(pos_z_AGN[0]), len(pos_z_gal[0]), len(pos_z_halo[0]) ))
+print("Halos: %d"%(len(hd_halo_z) ))
 
 """
 3. Finding halo pairs
 """
 if run_find_pairs:
-    for r in [14, 15]:
+    for r in [0]:
         print('\n ---- pairs within radius %.2f Mpc ---'%r_p[r])
-        pos_spherical = aimm.getSphericalCoord(hd_halo[conditions_halo])
+        pos_spherical = aimm.getSphericalCoord(hd_halo_z)
 
         # create tree
         tree_data = cKDTree(np.transpose(np.abs(pos_spherical)), leafsize=1000.0)
 
         # list of lists of all pair indicies per DM halo
-        pairs_idx = tree_data.query_ball_tree(tree_data, r=r_p[r], p=2) 
-        count_pairs, _ = cswl.countPairs(pairs_idx) 
+        all_mm_idx = tree_data.query_ball_tree(tree_data, r=r_p[r], p=2) 
+        count_pairs, _ = cswl.countPairs(all_mm_idx) 
         
         if major_mergers_only:
             # (1) choose only major pairs (major mergers)
-            all_mm_idx, count_major_pairs = cswl.majorMergerSelection(hd_halo[conditions_halo], pairs_idx)
-            pairs_idx = all_mm_idx
-            
-        # (2) choose only major pairs that satisfy the delta v criteria
-        all_dv_idx, count_dv_major_pairs = cswl.deltaVelSelection(hd_halo[conditions_halo], pairs_idx)
+            all_mm_idx, count_major_pairs = cswl.majorMergerSelection(hd_halo[conditions_halo], all_mm_idx)
+           
+        if delta_v_cut:            
+            # (2) choose only major pairs that satisfy the delta v criteria
+            all_dv_idx, count_dv_major_pairs = cswl.deltaVelSelection(hd_halo_z, all_mm_idx, mm = major_mergers_only)
         
-        # save file
-        np.save('Data/pairs_z%d/pairs_idx_r%d_mm%d_dv%d.npy'%(redshift_limit, r, mass_max, dv_cut), all_dv_idx, allow_pickle=True)
-
+        # save file based on the criteria applied        
+        if major_mergers_only and delta_v_cut:
+            np.save('Data/pairs_z%.1f/Major_pairs/pairs_idx_r%.1f_mm%d_dv%d.npy'%(redshift_limit, r, mass_max, dv_cut), all_dv_idx, allow_pickle=True)
+            print('\n --- Saved File --- ')
+            
+        if not major_mergers_only and delta_v_cut:
+            np.save('Data/pairs_z%.1f/dv_pairs/pairs_idx_r%.1f_dv%d.npy'%(redshift_limit, r, mass_max, dv_cut), all_dv_idx, allow_pickle=True)
+            print('\n --- Saved File --- ')
+            
+        # if you want to save all the pairs
+        if not major_mergers_only and not delta_v_cut:
+            np.save('Data/pairs_z%.1f/pairs_idx_r%d.npy'%(redshift_limit, r), all_mm_idx, allow_pickle=True)
+            
 """
 4. Studying the effect of Δ𝑡_merger on MM pairs
 
@@ -143,20 +139,18 @@ Now that all the pairs for the chosen cases of time since major mergers are comp
 where 𝑁𝑃 is the number of pairs and 𝑁 is the total number of objects from which pairs are chosen.
 """
 
-pairs_all = cswl.openPairsFiles(data_dir='Data/pairs_z2/Major_pairs/')
+if run_merger_pairs:
+    pairs_all = cswl.openPairsFiles(data_dir='Data/pairs_z%.1f/'%redshift_limit, mm=major_mergers_only)
+    
+    diff_t_mm_arr = np.load('Data/diff_t_mm_arr_z%.1f.npy'%(redshift_limit), allow_pickle=True)
+    
+    for dt_m in dt_m_arr[4:]:
+        count_t_mm_arr = []
 
-count_t_mm_arr_all_radius = np.zeros( (0, len(r_p) ) )
-
-for dt_m in [dt_m_arr[-1]]:
-    print('\n Choosing pairs with T_MM ~ %.1f'%dt_m)
-    count_t_mm_arr = []
-    
-    for r in range(len(r_p)): 
-        print('r_p = %.2f Mpc ...\n'%r_p[r])
-        _, count_t_mm = cswl.defineTimeSinceMergeCut(hd_halo[conditions_halo], pairs_all[0][r], cosmo, time_since_merger = dt_m)
-        count_t_mm_arr.append(count_t_mm)
-    
-    # save the counts for all radius bins for a given time since merger
-    count_t_mm_arr_all_radius = np.append(count_t_mm_arr_all_radius, [count_t_mm_arr], axis=0)
-    
-np.save('Data/pairs_z%d/Major_pairs/all_pairs_t_mm%.1f_r.npy'%(redshift_limit, dt_m), count_t_mm_arr_all_radius, allow_pickle=True)
+        for r in range(len(r_p)): 
+            print('\n ---- pairs within radius %.2f Mpc ---'%r_p[r])
+        
+            _, count_t_mm = cswl.defineTimeSinceMergeCut2(hd_halo_z, pairs_all[0][r], cosmo, diff_t_mm_arr, time_since_merger = dt_m, redshift_limit = redshift_limit)
+            count_t_mm_arr.append(count_t_mm)
+          
+        cswl.saveTmmFiles('mm and dv', dt_m, arr = count_t_mm_arr, redshift_limit = redshift_limit)

imported_files/Comparison_simulation_with_literature_data.py
@@ -119,7 +119,7 @@ def deltaVelSelection(hd_halo, all_mm_idx, dv_cut=500, mm = True):
     count_dv_major_mergers = countSelectedPairs(all_dv_idx, string = 'Delta v %d cut: '%dv_cut)
     return all_dv_idx, count_dv_major_mergers
 
-def openPairsFiles(data_dir = 'Data/Major_pairs/pairs_z2/', redshift_limit = 2, mass_max = 3, dv_cut = 500):
+def openPairsFiles(key, data_dir = 'Data/pairs_z2.0/', redshift_limit = 2, mass_max = 3, dv_cut = 500):
     """
     Function to open all the files with 
     """
@@ -128,12 +128,35 @@ def openPairsFiles(data_dir = 'Data/Major_pairs/pairs_z2/', redshift_limit = 2, 
     pairs_idx_all, n_pairs_arr = [], []
     
     for i in range(len(r_p)):
-        pairs_idx = np.load(data_dir+'pairs_idx_r%d_mm%d_dv%d.npy'%(i, mass_max, dv_cut), allow_pickle=True)
+        if key == 'mm and dv':
+            filename = 'Major_pairs/pairs_idx_r%.1f_mm%d_dv%d.npy'%(i, mass_max, dv_cut)
+        if key == 'dv':
+            filename = 'dv_pairs/pairs_idx_r%d_dv%d.npy'%(i, dv_cut)
+        if key == 'all':
+            filename = 'pairs_idx_r%d.npy'%(i)
+            
+        pairs_idx = np.load(data_dir+filename, allow_pickle=True)
         n_pairs = countSelectedPairs(pairs_idx, print_msg = False)
         
         n_pairs_arr.append(n_pairs)
         pairs_idx_all.append(pairs_idx)        
     return np.array([pairs_idx_all, n_pairs_arr], dtype=object)
+
+def saveTmmFiles(key, dt, arr , redshift_limit = 2):
+    """
+    Function decides where to save the tmm evaluated files
+    """
+    if key == 'mm and dv':
+        np.save('Data/pairs_z%.1f/Major_pairs/all_pairs_t_mm%.1f.npy'%(redshift_limit, dt), arr, allow_pickle=True)
+        
+    if key == 'dv':
+        np.save('Data/pairs_z%.1f/dv_pairs/all_pairs_t_mm%.1f.npy'%(redshift_limit, dt), arr, allow_pickle=True)
+
+    # if you want to save all the pairs
+    if key == 'all':
+        np.save('Data/pairs_z%.1f/all_pairs_t_mm%.1f.npy'%(redshift_limit, dt), arr, allow_pickle=True)
+    return
+
 
 def tmmToScale(cosmo, dt_m_arr):
     "Function to convert time time since MM array to scale factor of last MM"
@@ -183,18 +206,53 @@ def defineTimeSinceMergeCut(hd_obj, pairs_idx, cosmo, time_since_merger = 1):
     count_t_mm = countSelectedPairs(all_t_mm_idx, print_msg = False, string = 'T_mm = %d Gyr: '%time_since_merger)
     return all_t_mm_idx, count_t_mm
 
-def concatAllTmmFiles(dt_m_arr, redshift_limit=2, data_dir = 'Data/pairs_z2/'):
+def defineTimeSinceMergeCut2(hd_obj, pairs_idx, cosmo, diff_t_mm_arr, time_since_merger = 1, redshift_limit = 2):
+    """
+    Define the time since merger cut 
+    """
+    # object arr to save major pairs for every DM halo
+    all_t_mm_idx = []
+    
+    
+    for i, p in enumerate(pairs_idx): 
+        # list to save indicies of pairs classified as major mergers
+        t_mm_idx = []
+        diff_time = diff_t_mm_arr[i]
+        
+        # if there are more than 'self'-pairs
+        if len(p) >= 1:
+            for p_idx in p:                
+                diff_time_pair = diff_t_mm_arr[p_idx]
+                # only consider pairs that pass this time since merger-scale criterion
+                if (diff_time_pair <= time_since_merger) or  (diff_time <= time_since_merger):
+                    t_mm_idx.append(p_idx)
+                    
+        # save this info for the given halo in the object array
+        all_t_mm_idx.append(t_mm_idx)
+    
+    count_t_mm = countSelectedPairs(all_t_mm_idx, print_msg = False, string = 'T_mm = %d Gyr: '%time_since_merger)
+    return all_t_mm_idx, count_t_mm
+
+
+def concatAllTmmFiles(dt_m_arr, key, redshift_limit=2):
     """
     Function to concatenate all the files containing pairs for different T_mm criteria
     """
     r_p, _, _ = aimm.shellVolume()
     n_pairs_t_mm_all = np.zeros( (0, len(r_p) ) )
     
+    if key == 'mm and dv':
+        data_dir = 'Data/pairs_z%.1f/Major_pairs/'%redshift_limit
+    if key == 'dv':
+        data_dir = 'Data/pairs_z%.1f/dv_pairs/'%redshift_limit
+    if key == 'all':
+        data_dir = 'Data/pairs_z%.1f/'%redshift_limit    
+        
     for dt_m in dt_m_arr:
-        n_pairs_t_mm = np.load(data_dir+'all_pairs_t_mm%.1f_r.npy'%(dt_m), allow_pickle=True)
+        n_pairs_t_mm = np.load(data_dir+'all_pairs_t_mm%.1f.npy'%(dt_m), allow_pickle=True)
         
         # save the counts for all radius bins for a given time since merger
-        n_pairs_t_mm_all = np.append(n_pairs_t_mm_all, n_pairs_t_mm, axis=0)
+        n_pairs_t_mm_all = np.append(n_pairs_t_mm_all, [n_pairs_t_mm], axis=0)
     return n_pairs_t_mm_all
 
 def error(n_pairs):
@@ -221,7 +279,7 @@ def nPairsToFracPairs(hd_obj, all_pairs_vs_rp, redshift_limit = 2):
     
     # normalization
     total_num_pairs = len(hd_obj)    
-    N = total_num_pairs*(total_num_pairs - 1)
+    N = 2*total_num_pairs*(total_num_pairs - 1)
     
     f_pairs = num_pairs/(N*shell_volume)
     return f_pairs, error(num_pairs)

imported_files/plotting_aim03.py
@@ -172,14 +172,14 @@ def plotEllison(ax, r_p_E11, f_agn_E11, r_p_err_E11, f_agn_err_E11, color_E11, x
     ax.errorbar(r_p_E11, f_agn_E11, yerr = yerr, xerr = xerr, fmt='none', ecolor=mec_E11, capsize=2)
     return ax, np.array([r_p_E11, f_agn_E11, xerr, yerr], dtype=object)
 
-def plotAllLiteraturePlots(Satyapal_14, r_p_err_S14, f_agn_err_S14, r_p_L12, f_agn_L12, Liu_12_err, Silverman_11, r_p_err_Sil11, f_agn_err_Sil11, r_p_E11, f_agn_E11, r_p_err_E11, f_agn_err_E11, ax = None):
+def plotAllLiteraturePlots(Satyapal_14, r_p_err_S14, f_agn_err_S14, r_p_L12, f_agn_L12, Liu_12_err, Silverman_11, r_p_err_Sil11, f_agn_err_Sil11, r_p_E11, f_agn_E11, r_p_err_E11, f_agn_err_E11, ax = None, xmax= 150,ymax = 0.22):
     """
     Function plots all the data points obtained from literature 
     """
     if ax == None:
         fig, ax = plt.subplots(1,1,figsize=(7,6))
     
-    xmax, ymax = 150, 0.22
+    
     ax.set_xticks(ticks=np.arange(0, xmax, step=10), minor=True)
     ax.set_yticks(ticks=np.arange(0, ymax, step=1e-2), minor=True)
     

imported_files/plotting_cswl05.py
@@ -68,14 +68,17 @@ def plotFpairs(ax, r_p, f_pairs, f_pairs_err, label, color='r', errorbar = True)
     r_p_kpc, f_pairs = 1e3*r_p[1:], 1e9*f_pairs
 
     # plotting the results
-    ax.plot( r_p_kpc , f_pairs, 's', color=color, label = label)
+    ax.plot( r_p_kpc , f_pairs, 's', ls='--', color=color, label = label)
     if errorbar:
         ax.errorbar(r_p_kpc , np.array(f_pairs), yerr=f_pairs_err, ecolor='k', fmt='none', capsize=4.5)
     return ax
 
 
 def plotScaleMMdistribution(halo_m_scale_arr_all_r, cosmo, dt_m_arr):
-    ""
+    """
+    Function plots the number of objects in pairs as a function of the scale of last MM
+    --> the cuts on delta t_mm are overplotted to see the selection criterion
+    """
     fig, ax = plt.subplots(1,1,figsize=(7,6))
     bins = 20
     hist_all_r = np.zeros((0, bins))